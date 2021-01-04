a1ba079a4d38d5328379b8068ee479d6710511ab
yangshaotong <yangshaotong@int-yt.com>
2021-01-04 22:51:07 +0800

    master

--- 算法/1.两数之和.js
+++ 算法/1.两数之和.js
@@ -10,8 +10,14 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-
+var twoSum = function (nums, target) {
+    const numsLen = nums.length;
+    for (let i = 0; i < numsLen; i++) {
+        for (let j = i + 1; j < numsLen; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
 };
 // @lc code=end
-
